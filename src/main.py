664185e0ff8915695e12453ec3270f038a5d1176
nathangurnee <nathangurnee@gmail.com>
2021-12-07 21:21:24 -0800

    Implemented forward selection

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 from utils import *
 
-with open('../data/small_data_97.txt') as file:
-    data_file = file.read()
+if __name__ == '__main__':
+    with open('../data/small_data_86.txt') as file:
+        data_file = file.read()
 
-split_data = data_file.split('\n')
+    split_data = data_file.split('\n')
 
-data = []
-for item in split_data:
-    data.append(item.split())
-data.pop() # Removes the random empty list at the end
+    data = []
+    for item in split_data:
+        data.append(item.split())
+    data.pop() # Removes the random empty list at the end
 
-feature_search(data)
+    forward_selection(data)
--- src/utils.py
+++ src/utils.py
@@ -1,27 +1,68 @@
-import random
+from math import sqrt
+import copy
+
+def euclidean_distance(list1, list2):
+    sum = 0
+    for i in range(len(list1)):
+        sum += ((list2[i] - list1[i]) ** 2)
+    return sqrt(sum)
 
 def cross_validation(data, current_set, feature_to_add):
-    return random.randint(1, 10)
+    zero_data = copy.deepcopy(data)
+    for i in range(len(zero_data)):
+        for j in range(1, len(zero_data[i])):
+            if j not in current_set and j != feature_to_add:
+                zero_data[i][j] = 0
 
-def feature_search(data):
-    features = set()
+    number_correctly_classified = 0
 
-    for i in range(len(data)):
-        feature_to_add = ""
-        highest_accuracy = 0
-        print('On the ' + str(i + 1) + 'th level of the search tree')
-        for j in range(1, len(data[i])):
-            if j not in features:
-                print('--Considering adding the ' + str(j) + ' feature')
-                accuracy = cross_validation(data, features, float(data[i][j]))
+    for i in range(len(zero_data)):
+        object_to_classify = list(map(float, (zero_data[i])[1:len(zero_data[i])]))
+        label_object_to_classify = int(float(zero_data[i][0]))
 
-                if accuracy > highest_accuracy:
-                    highest_accuracy = accuracy
-                    feature_to_add = j
-                    
-        features.add(feature_to_add)
-        print('On level ' + str(i + 1) + ', added feature ' + str(feature_to_add) + ' to current set')
-        
-    features.pop() # Removes the empty string from the set
+        nearest_neighbor_distance = float('inf')
+        nearest_neighbor_location = float('inf')
+        nearest_neighbor_label = None
 
-    print(features)
+        for j in range(len(zero_data)):
+            if j != i:
+                object_to_compare = list(map(float, (zero_data[j])[1:len(zero_data[j])]))
+                distance = euclidean_distance(object_to_classify, object_to_compare)
+                
+                if distance < nearest_neighbor_distance:
+                    nearest_neighbor_distance = distance
+                    nearest_neighbor_location = j
+                    nearest_neighbor_label = int(float(zero_data[nearest_neighbor_location][0]))
+
+        if label_object_to_classify == nearest_neighbor_label:
+            number_correctly_classified += 1
+
+    return number_correctly_classified / len(zero_data)
+
+
+def forward_selection(data):
+    current_set_of_features = set()
+    best_accuracy_overall = 0
+    best_set_overall = set()
+
+    for i in range(len(data[0]) - 1):
+        feature_to_add_at_this_level = None
+        best_so_far_accuracy = 0
+    
+        for j in range(1, len(data[0])):
+            if j not in current_set_of_features:
+                accuracy = cross_validation(data, current_set_of_features, j)
+                if accuracy > best_so_far_accuracy:
+                    best_so_far_accuracy = accuracy
+                    feature_to_add_at_this_level = j;
+
+        current_set_of_features.add(feature_to_add_at_this_level)
+
+        if best_so_far_accuracy > best_accuracy_overall:
+            best_accuracy_overall = best_so_far_accuracy
+            best_set_overall = copy.copy(current_set_of_features)
+
+    format_accuracy = "{:.2f}".format(best_accuracy_overall)
+    print('The best feature subset is ' + str(best_set_overall) + ', which has an accuracy of ' + str(best_accuracy_overall * 100) + '%')
+
+
